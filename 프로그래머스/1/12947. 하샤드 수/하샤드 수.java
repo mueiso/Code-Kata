d717026b8ac40026530db96cec20b639663e3b55
mueiso <jcscene@gmail.com>
2025-05-21 21:38:40 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        int originalX = x; // x 값을 보존하기 위해 원본 값을 저장
+
+        // x의 각 자릿수 합 구하기
+        while (x > 0) {
+            sum += (x % 10); // 마지막 자릿수 더하기
+            x /= 10; // 마지막 자릿수 제거
+        }
+
+        // x가 자릿수 합으로 나누어 떨어지는지 확인
+        return originalX % sum == 0;
+    }
+}
