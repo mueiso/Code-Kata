c5cb8166022bff766b051f8653d46713420dbeb1
mueiso <jcscene@gmail.com>
2025-05-24 19:06:13 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.22 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        String[] words = s.split(" ", -1); // 공백 유지
+        
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            for (int j = 0; j < word.length(); j++) {
+                char ch = word.charAt(j);
+                if (j % 2 == 0) {
+                    answer.append(Character.toUpperCase(ch));
+                } else {
+                    answer.append(Character.toLowerCase(ch));
+                }
+            }
+            if (i < words.length - 1) {
+                answer.append(" "); // 단어 사이의 공백 추가
+            }
+        }
+        
+        return answer.toString();
+    }
+}
