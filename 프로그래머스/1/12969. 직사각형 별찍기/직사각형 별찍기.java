7657117117478d86b34922be427306f50d9385c7
mueiso <jcscene@gmail.com>
2025-05-24 19:05:09 +0900

    [level 1] Title: 직사각형 별찍기, Time: 676.58 ms, Memory: 90.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt(); // 가로 길이
+        int m = scanner.nextInt(); // 세로 길이
+        
+        scanner.close(); // 입력 종료
+        
+        // 세로(m) 줄 반복
+        for (int i = 0; i < m; i++) {
+            // 가로(n) 개의 '*' 출력
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println(); // 줄바꿈
+        }
+    }
+}
