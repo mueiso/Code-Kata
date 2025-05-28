168543b6e017a747148684fd31940caa64c69729
mueiso <jcscene@gmail.com>
2025-05-28 21:40:24 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,12 @@
+SELECT id,
+       email,
+       first_name,
+       last_name
+FROM DEVELOPERS dev
+WHERE dev.skill_code & 
+        (
+            SELECT BIT_OR(code)
+            FROM SKILLCODES
+            WHERE name IN ('Python', 'C#')
+        ) > 0
+ORDER BY id ASC;
