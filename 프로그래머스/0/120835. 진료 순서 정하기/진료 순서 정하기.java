09fcda387fecfdc4820a0a96fb643bff3a2674b7
mueiso <jcscene@gmail.com>
2025-06-06 00:46:25 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,24 +1,19 @@
-import java.util.Arrays;
-import java.util.HashMap;
-
 class Solution {
     public int[] solution(int[] emergency) {
-        int n = emergency.length;
-        int[] sorted = emergency.clone();
-        Arrays.sort(sorted);  // 오름차순 정렬
+        int[] answer = new int[emergency.length];
 
-        // 높은 응급도일수록 진료 순서(작은 숫자)가 되도록 매핑
-        HashMap<Integer, Integer> rankMap = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            // sorted[i]는 i번째로 작은 값. 진료 순서는 (n - i).
-            rankMap.put(sorted[i], n - i);
+        for(int i = 0; i < answer.length; i++){
+            if(answer[i] != 0){
+                continue;
+            }
+            int idx = 1;
+            for(int j = 0; j < answer.length; j++){
+                if(emergency[i] < emergency[j]){
+                    idx++;
+                }
+            }
+            answer[i] = idx;
         }
-
-        int[] answer = new int[n];
-        for (int i = 0; i < n; i++) {
-            answer[i] = rankMap.get(emergency[i]);
-        }
-
         return answer;
     }
 }
