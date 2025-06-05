0b088019a4ce6555292f9e62c64a93cc536830fc
mueiso <jcscene@gmail.com>
2025-06-06 00:42:24 +0900

    [level 0] Title: k의 개수, Time: 5650.21 ms, Memory: 381 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,17 +1,10 @@
 class Solution {
     public int solution(int i, int j, int k) {
-        int answer = 0;
-        String target = String.valueOf(k); // k를 문자열로 변환
-
-        for (int num = i; num <= j; num++) {
-            String strNum = String.valueOf(num);
-            for (int idx = 0; idx < strNum.length(); idx++) {
-                if (strNum.charAt(idx) == target.charAt(0)) {
-                    answer++;
-                }
-            }
+        String str = "";
+        for(int a = i; a <= j; a++) {
+            str += a+"";
         }
 
-        return answer;
+        return str.length() - str.replace(k+"", "").length();
     }
 }
