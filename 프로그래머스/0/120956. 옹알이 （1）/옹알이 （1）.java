3012e7d5519740d021a54d7b9c931251e1460661
mueiso <jcscene@gmail.com>
2025-06-05 19:52:31 +0900

    [level 0] Title: 옹알이 (1), Time: 6.23 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,50 +1,11 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        
-        for (String s : babbling) {
-            if (isValidPronunciation(s)) {
+        for(int i=0; i<babbling.length; i++){
+            if(babbling[i].matches("^(aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma))+$")){
                 answer++;
             }
         }
-        
         return answer;
-    }
-    
-    private boolean isValidPronunciation(String s) {
-        int i = 0;
-        boolean usedAya = false;
-        boolean usedYe = false;
-        boolean usedWoo = false;
-        boolean usedMa = false;
-        
-        while (i < s.length()) {
-            // Check for "aya"
-            if (!usedAya && i + 3 <= s.length() && s.startsWith("aya", i)) {
-                usedAya = true;
-                i += 3;
-            }
-            // Check for "ye"
-            else if (!usedYe && i + 2 <= s.length() && s.startsWith("ye", i)) {
-                usedYe = true;
-                i += 2;
-            }
-            // Check for "woo"
-            else if (!usedWoo && i + 3 <= s.length() && s.startsWith("woo", i)) {
-                usedWoo = true;
-                i += 3;
-            }
-            // Check for "ma"
-            else if (!usedMa && i + 2 <= s.length() && s.startsWith("ma", i)) {
-                usedMa = true;
-                i += 2;
-            }
-            // If none match, it's invalid
-            else {
-                return false;
-            }
-        }
-        
-        return true;
     }
 }
