ac4d63760096b5b7437434c38d316687d2b1a895
mueiso <jcscene@gmail.com>
2025-06-06 00:44:54 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.05 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -1,23 +1,16 @@
-import java.util.Arrays;
-
 class Solution {
     public String solution(String s) {
-        int[] freq = new int[26];
-        
-        // 1. 각 문자 빈도 계산
-        for (char c : s.toCharArray()) {
-            freq[c - 'a']++;
+        int[] alpha = new int[26];
+        for(char c : s.toCharArray()){
+            alpha[c - 'a']++;
         }
-        
-        // 2. 빈도가 1인 문자만 골라서 배열에 담음
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] == 1) {
-                sb.append((char) (i + 'a'));
+
+        StringBuilder answer = new StringBuilder();
+        for(int i = 0; i < 26; i++){
+            if(alpha[i] == 1){
+                answer.append((char)(i + 'a'));
             }
         }
-        
-        // 3. StringBuilder에 이미 알파벳 순으로 들어갔으므로 그대로 반환
-        return sb.toString();
+        return answer.toString();
     }
 }
