7457117b9774aa80983ebe5149df11c9e5f56af8
mueiso <jcscene@gmail.com>
2025-06-06 00:29:24 +0900

    [level 0] Title: 369게임, Time: 7.73 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -1,13 +1,12 @@
 class Solution {
     public int solution(int order) {
         int answer = 0;
-        String str = String.valueOf(order);
 
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if (ch == '3' || ch == '6' || ch == '9') {
-                answer++;
-            }
+        String str = order+"";
+
+        for(int i=0; i<str.length(); i++){
+            char c = str.charAt(i);
+            if(c=='3'||c=='6'||c=='9') answer++;
         }
 
         return answer;
