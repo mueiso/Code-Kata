0579035bb891efc68b17e06cb7c05b370b0ed212
mueiso <jcscene@gmail.com>
2025-06-06 00:40:58 +0900

    [level 0] Title: A로 B 만들기, Time: 0.85 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -1,16 +1,11 @@
 import java.util.Arrays;
-
 class Solution {
     public int solution(String before, String after) {
-        // 문자열을 문자 배열로 변환
-        char[] beforeArr = before.toCharArray();
-        char[] afterArr = after.toCharArray();
+        char[] a = before.toCharArray();
+        char[] b = after.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
 
-        // 정렬
-        Arrays.sort(beforeArr);
-        Arrays.sort(afterArr);
-
-        // 정렬된 배열이 같으면 1, 다르면 0
-        return Arrays.equals(beforeArr, afterArr) ? 1 : 0;
+        return new String(a).equals(new String(b)) ? 1 :0;
     }
 }
