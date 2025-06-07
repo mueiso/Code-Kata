2b0bd8e1428da856aa94e223d72a06f1d34ce272
mueiso <jcscene@gmail.com>
2025-06-07 18:14:09 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.63 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,15 +1,13 @@
 class Solution {
     public long solution(String numbers) {
-        // 숫자 단어와 대응되는 숫자 문자열을 배열로 준비
-        String[] numWords = {"zero", "one", "two", "three", "four",
-                             "five", "six", "seven", "eight", "nine"};
 
-        // numbers 문자열을 각 단어별로 숫자 문자열로 바꾸기
-        for (int i = 0; i < 10; i++) {
-            numbers = numbers.replaceAll(numWords[i], Integer.toString(i));
+            String[] numbers_arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+            for (int i = 0; i < numbers_arr.length; i++) {
+                numbers = numbers.replaceAll(numbers_arr[i], String.valueOf(i));
+            }
+
+            long answer = Long.parseLong(numbers);
+            return answer;
         }
-
-        // 변환된 문자열을 long 타입으로 변환 후 반환
-        return Long.parseLong(numbers);
-    }
 }
