37128a8e11d1cb82d1731f5b9509bb1ed84fd4a0
mueiso <jcscene@gmail.com>
2025-06-07 17:59:55 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.23 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,24 +1,13 @@
 class Solution {
     public int solution(String my_string) {
         int answer = 0;
-        StringBuilder numBuf = new StringBuilder();
-        
-        for (int i = 0; i < my_string.length(); i++) {
-            char c = my_string.charAt(i);
-            if (Character.isDigit(c)) {
-                numBuf.append(c);
-            } else {
-                if (numBuf.length() > 0) {
-                    answer += Integer.parseInt(numBuf.toString());
-                    numBuf.setLength(0);
-                }
-            }
+
+        String[] str = my_string.replaceAll("[a-zA-Z]", " ").split(" ");
+
+        for(String s : str){
+            if(!s.equals("")) answer += Integer.valueOf(s);
         }
-        // 마지막에 버퍼에 남은 숫자가 있으면 더하기
-        if (numBuf.length() > 0) {
-            answer += Integer.parseInt(numBuf.toString());
-        }
-        
+
         return answer;
     }
 }
