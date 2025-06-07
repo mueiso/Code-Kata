9bc6839d67f116dde8d6b7c37fc1372ba630b427
mueiso <jcscene@gmail.com>
2025-06-08 00:31:17 +0900

    [level 0] Title: 외계어 사전, Time: 0.09 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -1,23 +1,12 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(String[] spell, String[] dic) {
-        // spell 배열을 하나의 문자열로 만들고, 정렬된 상태로 저장
-        char[] spellChars = String.join("", spell).toCharArray();
-        Arrays.sort(spellChars);
-        String sortedSpell = new String(spellChars);
-        
-        for (String word : dic) {
-            if (word.length() != spell.length) continue;
-            char[] wordChars = word.toCharArray();
-            Arrays.sort(wordChars);
-            String sortedWord = new String(wordChars);
-            
-            if (sortedSpell.equals(sortedWord)) {
-                return 1;  // 일치하는 단어가 존재함
+        for(int i=0;i<dic.length;i++){
+            int answer = 0;
+            for(int j=0;j<spell.length;j++){
+                if(dic[i].contains(spell[j])) answer ++;
             }
+            if(answer==spell.length) return 1;
         }
-        
-        return 2;  // 일치하는 단어가 없음
+        return 2;
     }
 }
