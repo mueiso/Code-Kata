c25520572ee0b1fd87a43add7162a33cdff71fcc
mueiso <jcscene@gmail.com>
2025-06-08 00:28:11 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.06 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -1,28 +1,65 @@
+import java.lang.Math;
 class Solution {
+    private String up = "up";
+    private String down = "down";
+    private String left = "left";
+    private String right = "right";
+    private int xPos = 0;
+    private int yPos = 1;
+    private int maxWidth = 0;
+    private int maxHeight= 0;
+    int[] answer = {0, 0};
+
     public int[] solution(String[] keyinput, int[] board) {
-        int x = 0;
-        int y = 0;
-        
-        int maxX = board[0] / 2;
-        int maxY = board[1] / 2;
-        
-        for (String key : keyinput) {
-            switch (key) {
-                case "left":
-                    if (x > -maxX) x--;
-                    break;
-                case "right":
-                    if (x < maxX) x++;
-                    break;
-                case "up":
-                    if (y < maxY) y++;
-                    break;
-                case "down":
-                    if (y > -maxY) y--;
-                    break;
-            }
+        maxWidth = board[xPos] / 2;
+        maxHeight = board[yPos] / 2;
+        for (String moveKey : keyinput) {
+            move(moveKey);
         }
-        
-        return new int[]{x, y};
+        return answer;
+    }
+
+    private void move(String key) {
+        if (up.equals(key)) {
+            // up
+            moveUp();
+        } else if (down.equals(key)) {
+            // down
+            moveDown();
+        } else if (left.equals(key)) {
+            // left
+            moveLeft();
+        } else {
+            // right
+            moveRight();
+        }
+    }
+
+    private void moveUp() {
+        int top = Math.abs(maxHeight);
+        if (answer[yPos] + 1 <= top) {
+            answer[yPos]++;
+        };
+    }
+
+    private void moveDown() {
+        int bottom = Math.abs(maxHeight) * -1;
+        if (answer[yPos] - 1 >= bottom) {
+            answer[yPos]--;
+        };
+    }
+
+    private void moveLeft() {
+        int left = Math.abs(maxWidth) * -1;
+        if (answer[xPos] - 1 >= left) {
+            answer[xPos]--;
+        };
+    }
+
+    private void moveRight() {
+        int right = Math.abs(maxWidth);
+        if (answer[xPos] + 1 <= right) {
+            answer[xPos]++;
+        };
     }
 }
