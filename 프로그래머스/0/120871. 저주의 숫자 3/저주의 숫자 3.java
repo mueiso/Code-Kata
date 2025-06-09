07d29db87fe0ee5eed1305d206c615f2d791e7e9
mueiso <jcscene@gmail.com>
2025-06-09 14:58:34 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.26 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -1,16 +1,14 @@
 class Solution {
     public int solution(int n) {
-        int count = 0; // 현재까지 찾은 3x 마을 숫자 개수
-        int number = 0; // 실제 숫자를 증가시키며 찾음
+        int answer = 0;
 
-        while (count < n) {
-            number++;
-            // 3의 배수도 아니고, 숫자에 3이 포함되지 않는 경우만 카운트
-            if (number % 3 != 0 && !String.valueOf(number).contains("3")) {
-                count++;
+        for (int i = 1; i <= n; i++) {
+            answer++;
+            if (answer % 3 == 0 || String.valueOf(answer).contains("3")) {
+                i--;
             }
         }
 
-        return number;
+        return answer;
     }
 }
