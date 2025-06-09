d32608a9997d883689b0120e889ae4b7c1be6022
mueiso <jcscene@gmail.com>
2025-06-10 00:22:34 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -1,19 +1,16 @@
 class Solution {
     public int solution(int a, int b) {
-        // a/b를 기약분수로 만들기 위해 최대공약수로 나눔
-        int gcd = getGCD(a, b);
-        b /= gcd;
+        int gcd = gcd(a, b);
+        int denom = b / gcd;
 
-        // 분모에 2와 5 이외의 소인수가 있다면 무한소수
-        while (b % 2 == 0) b /= 2;
-        while (b % 5 == 0) b /= 5;
+        while (denom % 2 == 0) denom /= 2;
+        while (denom % 5 == 0) denom /= 5;
 
-        return b == 1 ? 1 : 2;
+        return (denom == 1) ? 1 : 2;
     }
 
-    // 최대공약수(GCD)를 구하는 유클리드 호제법
-    private int getGCD(int a, int b) {
+    private int gcd(int a, int b) {
         if (b == 0) return a;
-        return getGCD(b, a % b);
+        return gcd(b, a % b);
     }
 }
