bb49308d9cab1c79858de332f9db44aa4807fd6c
mueiso <jcscene@gmail.com>
2025-06-09 14:53:35 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,14 +1,10 @@
 class Solution {
     public int solution(int chicken) {
-        int service = 0;
-        int coupon = chicken;
-
-        while (coupon >= 10) {
-            int extra = coupon / 10;     // 서비스 치킨 개수
-            service += extra;
-            coupon = extra + (coupon % 10);  // 남은 쿠폰
+        int answer = chicken/9;
+        if(chicken>1&&chicken%9==0){
+            answer--;
         }
 
-        return service;
+        return answer;
     }
 }
