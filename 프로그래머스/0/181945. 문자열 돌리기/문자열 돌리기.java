81163ecc1cd72a0ef3f310bc9dac7c4b3895c97a
mueiso <jcscene@gmail.com>
2025-07-09 19:06:01 +0900

    [level 0] Title: 문자열 돌리기, Time: 254.63 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -3,10 +3,8 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-
-        for (int i = 0; i < str.length(); i++) {
-            System.out.println(str.charAt(i));
-        }
+        String a = sc.next();
+        for (char ch : a.toCharArray())
+            System.out.println(ch);
     }
 }
