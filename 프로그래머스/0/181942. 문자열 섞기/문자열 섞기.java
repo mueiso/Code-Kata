0449995d25528c226f6faf14fe8403f0fc50df94
mueiso <jcscene@gmail.com>
2025-07-10 00:32:38 +0900

    [level 0] Title: 문자열 섞기, Time: 13.09 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,12 +1,12 @@
 class Solution {
     public String solution(String str1, String str2) {
-        StringBuilder sb = new StringBuilder();
+        String answer = "";
 
-        for (int i = 0; i < str1.length(); i++) {
-            sb.append(str1.charAt(i));
-            sb.append(str2.charAt(i));
+        for(int i = 0; i < str1.length(); i++){
+            answer+= str1.charAt(i);
+            answer+= str2.charAt(i);
         }
 
-        return sb.toString();
+        return answer;
     }
 }
