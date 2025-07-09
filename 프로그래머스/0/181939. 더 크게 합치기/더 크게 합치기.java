4141b82a20df5d6ba85d0d46e1a702ebe3d899ff
mueiso <jcscene@gmail.com>
2025-07-10 00:43:34 +0900

    [level 0] Title: 더 크게 합치기, Time: 12.58 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,9 +1,11 @@
 class Solution {
     public int solution(int a, int b) {
         
-        int ab = Integer.parseInt("" + a + b);
-        int ba = Integer.parseInt("" + b + a);
-        
-        return Math.max(ab, ba);
+        int answer = 0;
+        int aLong = Integer.parseInt(""+a+b);
+        int bLong = Integer.parseInt(""+b+a);
+        answer = aLong > bLong ? aLong : bLong;
+
+        return answer;
     }
 }
