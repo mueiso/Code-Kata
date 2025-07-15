6c7bb9495a69170b370ab761ae66900fdc7307d8
mueiso <jcscene@gmail.com>
2025-07-15 16:05:37 +0900

    [level 0] Title: 코드 처리하기, Time: 8.05 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,23 +1,20 @@
 class Solution {
     public String solution(String code) {
         
-        StringBuilder ret = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
         int mode = 0;
-
         for (int i = 0; i < code.length(); i++) {
-            char ch = code.charAt(i);
+            char current = code.charAt(i);
+            if (current == '1') {
+                mode = mode == 0 ? 1 : 0;
+                continue;
+            }
 
-            if (ch == '1') {
-                mode = 1 - mode; // mode 전환: 0->1, 1->0
-            } else {
-                if (mode == 0 && i % 2 == 0) {
-                    ret.append(ch);
-                } else if (mode == 1 && i % 2 == 1) {
-                    ret.append(ch);
-                }
+            if (i % 2 == mode) {
+                answer.append(current);
             }
         }
-
-        return ret.length() == 0 ? "EMPTY" : ret.toString();
+        
+        return answer.length() == 0 ? "EMPTY" : answer.toString();
     }
 }
