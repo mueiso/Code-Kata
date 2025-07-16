e7f9926648d9bb604a31fe00035dd2cc8bc69468
mueiso <jcscene@gmail.com>
2025-07-16 19:27:19 +0900

    [level 0] Title: 5명씩, Time: 0.03 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -1,15 +1,13 @@
-import java.util.*;
-
 class Solution {
     
     public String[] solution(String[] names) {
         
-        List<String> result = new ArrayList<>();
-
-        for (int i = 0; i < names.length; i += 5) {
-            result.add(names[i]);  // 각 그룹의 첫 번째 사람
-        }
-
-        return result.toArray(new String[0]);
+        int idx = 0;
+        String[] answer = new String[names.length % 5 == 0 ? names.length / 5 : names.length / 5 + 1];
+        
+        for (int i = 0;i < names.length;i+=5)
+            answer[idx++] = names[i];
+        
+        return answer;
     }
 }
