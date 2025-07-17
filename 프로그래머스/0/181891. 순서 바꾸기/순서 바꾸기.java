95706090a868b6290b1f120b9dc290b796d970a2
mueiso <jcscene@gmail.com>
2025-07-17 12:25:59 +0900

    [level 0] Title: 순서 바꾸기, Time: 4.60 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,22 +1,9 @@
-import java.util.*;
+import java.util.stream.IntStream;
 
 class Solution {
     
     public int[] solution(int[] num_list, int n) {
         
-        int[] answer = new int[num_list.length];
-        int idx = 0;
-
-        // n번째 이후 원소 먼저 넣기
-        for (int i = n; i < num_list.length; i++) {
-            answer[idx++] = num_list[i];
-        }
-
-        // 그다음 n번째까지의 원소 넣기
-        for (int i = 0; i < n; i++) {
-            answer[idx++] = num_list[i];
-        }
-
-        return answer;
+        return IntStream.range(0, num_list.length).map(i -> num_list[(i + n) % num_list.length]).toArray();
     }
 }
