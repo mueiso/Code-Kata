f532ef7a9570f7380444339153e29f20d11ab9e7
mueiso <jcscene@gmail.com>
2025-07-21 17:44:11 +0900

    [level 0] Title: 수 조작하기 1, Time: 8.18 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -2,18 +2,23 @@ class Solution {
     
     public int solution(int n, String control) {
         
-        for (char c : control.toCharArray()) {
-            if (c == 'w') {
-                n += 1;
-            } else if (c == 's') {
-                n -= 1;
-            } else if (c == 'd') {
-                n += 10;
-            } else if (c == 'a') {
-                n -= 10;
+        int answer = n;
+
+        // control 문자열을 문자배열로 변환하여 하나씩 문자 c로 반복 (예: "wsd" → ['w', 's', 'd'])
+        for(char c : control.toCharArray()) {
+            switch(c) {
+                case 'w': answer += 1;
+                    break;
+                case 's': answer -= 1;
+                    break;
+                case 'd': answer += 10;
+                    break;
+                case 'a': answer -= 10;
+                    break;
+                default:break;
             }
         }
-        
-        return n;
+
+        return answer;
     }
 }
