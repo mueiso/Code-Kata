3eaadc6763417b6a628962b8f965ba4310deebbf
mueiso <jcscene@gmail.com>
2025-07-24 02:29:22 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 3.41 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,23 +1,34 @@
+import java.util.Arrays;
+
 class Solution {
+    
     public int[] solution(int[] arr, int[][] queries) {
+
+        // queries.length 크기의 정답 배열 생성
         int[] answer = new int[queries.length];
+        // 처음에는 모든 값을 -1로 초기화 (조건을 만족하는 값이 없을 경우를 대비)
+        Arrays.fill(answer, -1);
 
-        for (int q = 0; q < queries.length; q++) {
-            int s = queries[q][0];
-            int e = queries[q][1];
-            int k = queries[q][2];
+        /*
+         * 각 쿼리 [s, e, k]를 추출
+         * arr[s]부터 arr[e]까지 순회하면서 k보다 큰 수 중 가장 작은 값을 찾음
+         */
+        for (int idx = 0; idx < queries.length; idx++) {
+            int[] query = queries[idx];
+            int s = query[0], e = query[1], k = query[2];
 
-            int min = Integer.MAX_VALUE;
-            boolean found = false;
-
+            /*
+             * arr[i] > k 조건을 만족하면:
+                answer[idx]가 아직 -1이면 arr[i]로 초기화
+                이미 값이 있다면 Math.min()을 통해 더 작은 값으로 갱신
+             * 이 구조 덕분에 별도의 min 변수 없이 answer 배열만으로 최소값을 관리할 수 있음
+             */
             for (int i = s; i <= e; i++) {
-                if (arr[i] > k && arr[i] < min) {
-                    min = arr[i];
-                    found = true;
+                if (k < arr[i]) {
+                    answer[idx] = answer[idx] == -1 ? arr[i] : Math.min(answer[idx], arr[i]);
                 }
             }
 
-            answer[q] = found ? min : -1;
         }
 
         return answer;
