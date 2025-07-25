eb6ccf0f475d056fba609cccad867d78ce3d0bfc
mueiso <jcscene@gmail.com>
2025-07-25 20:38:24 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 4.46 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -1,16 +1,14 @@
 class Solution {
     
+    // queries[i] = [s, e, k] 이고, 인덱스 j가 s <= j <= e 범위이며 j % k == 0이면 arr[j] += 1 수행
     public int[] solution(int[] arr, int[][] queries) {
         
-        for (int[] query : queries) {
-            int s = query[0];
-            int e = query[1];
-            int k = query[2];
-
-            for (int i = s; i <= e; i++) {
-                if (i % k == 0) {
-                    arr[i]++;
-                }
+        // i는 현재 쿼리의 인덱스
+        for (int i = 0; i < queries.length; i++) {
+            // j는 현재 쿼리의 시작 인덱스 s부터 종료 인덱스 e까지 반복
+            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
+                // j가 k의 배수이면 arr[j] 값을 1 증가시킴
+                if (j % queries[i][2] == 0) arr[j] += 1;
             }
         }
         
