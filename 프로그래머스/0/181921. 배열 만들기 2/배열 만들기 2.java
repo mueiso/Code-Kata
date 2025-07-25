6a09c3418a93d3b1d2c4e5c8fa3d291cf6c4a914
mueiso <jcscene@gmail.com>
2025-07-26 02:54:38 +0900

    [level 0] Title: 배열 만들기 2, Time: 2.71 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -1,30 +1,17 @@
-import java.util.*;
+import java.util.ArrayList;
 
 class Solution {
     
     public int[] solution(int l, int r) {
-        
-        List<Integer> result = new ArrayList<>();
 
-        for (int i = l; i <= r; i++) {
-            String s = String.valueOf(i);
-            boolean isValid = true;
+        ArrayList<Integer> list = new ArrayList<>();
 
-            for (char c : s.toCharArray()) {
-                if (c != '0' && c != '5') {
-                    isValid = false;
-                    break;
-                }
-            }
-
-            if (isValid) result.add(i);
+        for (int i = 1; i < 64; i++) {
+            int num = Integer.parseInt(Integer.toBinaryString(i)) * 5;
+            if (l <= num && num <= r)
+                list.add(num);
         }
 
-        if (result.isEmpty()) {
-            return new int[]{-1};
-        }
-
-        // 리스트를 배열로 변환
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        return list.isEmpty() ? new int[] { -1 } : list.stream().mapToInt(i -> i).toArray();
     }
 }
