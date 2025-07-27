249856e6355fcff90f1a5f2520d3237fbf52bf5f
mueiso <jcscene@gmail.com>
2025-07-27 18:03:25 +0900

    [level 0] Title: 카운트 업, Time: 2.30 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,15 +1,9 @@
+import java.util.stream.IntStream;
+
 class Solution {
     
-    public int[] solution(int start_num, int end_num) {
+    public int[] solution(int start, int end) {
         
-        // start_num부터 end_num까지의 개수만큼 배열 생성
-        int[] answer = new int[end_num - start_num + 1];
-
-        // start_num부터 end_num까지 순차적으로 값을 채워 넣기
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = start_num + i;
-        }
-
-        return answer;
+        return IntStream.rangeClosed(start, end).toArray();
     }
 }
