a87b1bc1829df9477b53f292de3fed7c2babd216
mueiso <jcscene@gmail.com>
2025-07-30 15:57:37 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.03 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -0,0 +1,19 @@
+class Solution {
+    
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        
+        return (x1 || x2) && (x3 || x4);
+    }
+}
+
+/*
+ * ∨ : OR (또는) 연산 (||로 표현)
+ * ∧ : AND (그리고) 연산 (&&로 표현)
+ * 즉, Java에서 표현하면: (x1 ∨ x2) ∧ (x3 ∨ x4) → (x1 || x2) && (x3 || x4)
+ */
+
+/*
+ * x1 || x2: x1 또는 x2 중 하나라도 true면 true
+ * x3 || x4: x3 또는 x4 중 하나라도 true면 true
+ * 두 결과를 && 연산으로 연결: 둘 다 true일 때만 전체 결과가 true
+ */
