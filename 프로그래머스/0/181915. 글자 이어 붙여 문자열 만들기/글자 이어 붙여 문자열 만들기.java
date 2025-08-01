d2df4122b866d5cb52cf569fc79e5050cf77fc1b
mueiso <jcscene@gmail.com>
2025-08-01 14:39:45 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.11 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.java
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.java
@@ -1,16 +1,15 @@
 class Solution {
     
     public String solution(String my_string, int[] index_list) {
-        
-        // StringBuilder는 문자열을 효율적으로 이어 붙이기 위해 사용
+
+        // 결과 문자열을 효율적으로 만들기 위해 StringBuilder 사용
         StringBuilder sb = new StringBuilder();
 
-        // index_list의 각 인덱스를 순회하면서 해당하는 문자를 sb에 추가
-        for (int index : index_list) {
-            sb.append(my_string.charAt(index));
-        }
+        // index_list의 각 인덱스를 순회하면서 해당 인덱스의 문자를 추출해 StringBuilder에 추가
+        for (int idx : index_list)
+            sb.append(my_string.charAt(idx)); // my_string의 idx 번째 문자를 sb에 추가
 
-        // 최종 문자열 반환
+        // 최종적으로 만들어진 문자열을 반환
         return sb.toString();
     }
 }
