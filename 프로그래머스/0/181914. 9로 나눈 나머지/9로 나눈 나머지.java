8099a3de4946f1b8ca3f871bca38f9968ea0a03b
mueiso <jcscene@gmail.com>
2025-08-02 23:08:08 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 5.14 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -2,16 +2,14 @@ class Solution {
     
     public int solution(String number) {
 
-        // 모든 자릿수의 합을 저장할 변수
-        int sum = 0;
-
-        // 문자열로 주어진 숫자의 각 문자를 순회
-        for (char c : number.toCharArray()) {
-            // 각 문자를 숫자로 변환하여 sum에 누적
-            sum += c - '0';
-        }
-
-        // 자릿수의 합을 9로 나눈 나머지를 반환
-        return sum % 9;
+        /*
+         * 문자열 number를 정수 스트림으로 변환하고,
+         * 각 문자에서 '0'을 빼서 정수로 바꾼 뒤, 각 자리수를 9로 나눈 나머지를 누적합한 후,
+         * 그 결과를 다시 9로 나눈 나머지를 반환한다.
+         */
+        return number
+                .chars()                                // 문자열의 각 문자를 IntStream으로 변환 (유니코드 값)
+                .map(c -> (c - '0') % 9)                // 문자 '0'을 빼서 숫자로 변환 후 9로 나눈 나머지 계산
+                .sum() % 9;                             // 모든 자리 수의 (mod 9) 결과를 더한 뒤, 다시 9로 나눈 나머지 반환
     }
 }
