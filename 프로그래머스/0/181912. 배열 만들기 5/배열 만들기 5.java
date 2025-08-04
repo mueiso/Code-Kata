cd32230d3a845ed244cea756dcfd396544cd2af2
mueiso <jcscene@gmail.com>
2025-08-04 15:16:04 +0900

    [level 0] Title: 배열 만들기 5, Time: 9.17 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -1,26 +1,36 @@
-import java.util.*;
+import java.util.ArrayList;
 
 class Solution {
-    
-    public int[] solution(String[] intStrs, int k, int s, int l) {
-        
-        List<Integer> list = new ArrayList<>();
 
-        // intStrs 배열의 각 문자열에 대해 반복
-        for (String str : intStrs) {
-            // s번째 인덱스부터 길이 l만큼 부분 문자열을 추출
-            String part = str.substring(s, s + l);
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+
+        // 조건을 만족하는 정수를 담을 리스트 생성
+        ArrayList<Integer> list = new ArrayList<>();
+
+        // 주어진 문자열 배열을 순회
+        for (int i = 0; i < intStrs.length; i++) {
+
+            // 각 문자열에서 s번째 인덱스부터 길이 l만큼 부분 문자열을 추출
+            String subStr = intStrs[i].substring(s, s + l);
 
             // 부분 문자열을 정수로 변환
-            int num = Integer.parseInt(part);
+            int tmp = Integer.parseInt(subStr);
 
-            // 변환한 숫자가 k보다 크면 결과 리스트에 추가
-            if (num > k) {
-                list.add(num);
+            // 변환한 정수가 k보다 클 경우에만 리스트에 추가
+            if (tmp > k) {
+                list.add(tmp);
             }
         }
 
-        // 리스트를 int[] 배열로 변환하여 반환
-        return list.stream().mapToInt(i -> i).toArray();
+        // 리스트에 저장된 결과를 int 배열로 변환
+        int[] answer = new int[list.size()];
+
+        // 리스트에서 하나씩 꺼내서 배열에 복사
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+
+        // 최종 결과 반환
+        return answer;
     }
 }
