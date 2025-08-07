a41cb8ea10b9c51a3de85da10a71a19ade57d921
mueiso <jcscene@gmail.com>
2025-08-07 14:16:15 +0900

    [level 0] Title: 접미사 배열, Time: 0.66 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -1,20 +1,22 @@
 import java.util.Arrays;
 
 class Solution {
-    
+
     public String[] solution(String my_string) {
         
-        // 문자열 길이만큼 접미사 배열 생성
-        String[] suffixes = new String[my_string.length()];
+        // 결과를 저장할 배열 선언: 문자열의 길이만큼 접미사가 있으므로 같은 길이로 배열 생성
+        String[] answer = new String[my_string.length()];
 
-        // 각 인덱스부터 시작하는 접미사를 배열에 저장
-        for (int i = 0; i < my_string.length(); i++) {
-            suffixes[i] = my_string.substring(i);
+        // my_string의 각 인덱스부터 끝까지 잘라서 접미사를 만든다
+        for (int i = 0; i < answer.length; i++) {
+            // substring(i, my_string.length())는 인덱스 i부터 문자열 끝까지의 부분 문자열을 의미
+            answer[i] = my_string.substring(i, my_string.length());
         }
 
-        // 접미사 배열을 사전순으로 정렬
-        Arrays.sort(suffixes);
+        // 모든 접미사를 사전 순으로 정렬
+        Arrays.sort(answer);
 
-        return suffixes;
+        // 정렬된 접미사 배열 반환
+        return answer;
     }
 }
