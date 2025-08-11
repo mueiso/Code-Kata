1017fff5b148e8f0aaa431ce76d7d5d0ade9d819
mueiso <jcscene@gmail.com>
2025-08-11 18:44:41 +0900

    [level 0] Title: 문자열 뒤집기, Time: 16.97 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -3,22 +3,20 @@ class Solution {
     public String solution(String my_string, int s, int e) {
         
         /*
-         * StringBuilder를 사용하면 문자열 일부를 쉽게 뒤집을 수 있음
-         * 1. s 이전의 부분은 그대로 둔다.
-         * 2. s ~ e 구간은 뒤집는다.
-         * 3. e 이후의 부분을 그대로 이어 붙인다.
+         * my_string의 s ~ e 구간을 잘라내어 StringBuilder에 저장
+         * StringBuilder를 사용하면 reverse() 메서드로 쉽게 문자열을 뒤집을 수 있음
          */
+        StringBuilder answer = new StringBuilder(my_string.substring(s, e + 1));
         
-        // 1. s 이전 문자열
-        String prefix = my_string.substring(0, s);
+        // s ~ e 구간의 문자열을 뒤집음
+        answer.reverse();
         
-        // 2. s~e 구간을 뒤집기 위해 StringBuilder 사용
-        String reversedPart = new StringBuilder(my_string.substring(s, e + 1)).reverse().toString();
-        
-        // 3. e 이후 문자열
-        String suffix = my_string.substring(e + 1);
-        
-        // 세 부분을 합쳐서 반환
-        return prefix + reversedPart + suffix;
+        /*
+         * 최종 결과 생성:
+         * 1. my_string의 앞부분 (0 ~ s-1)
+         * 2. 뒤집은 문자열(answer)
+         * 3. my_string의 뒷부분 (e+1 ~ 끝)
+         */
+        return my_string.substring(0, s) + answer + my_string.substring(e + 1);
     }
 }
