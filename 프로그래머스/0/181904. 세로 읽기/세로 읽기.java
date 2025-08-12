1d216110c4459b92fcb4a4341222e4fe9924f756
mueiso <jcscene@gmail.com>
2025-08-12 15:46:34 +0900

    [level 0] Title: 세로 읽기, Time: 0.18 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -1,26 +1,36 @@
 class Solution {
-
+    
     public String solution(String my_string, int m, int c) {
         
-        // 결과 문자열을 저장할 변수
-        String answer = "";
-
         /*
-         * m : 한 줄에 들어가는 글자 수
-         * c : 선택할 열 번호 (1부터 시작)
-         * 
-         * i = c - 1 → 배열 인덱스는 0부터 시작하므로 -1 해줌
-         * 이후 m씩 건너뛰면서 해당 위치의 문자 선택
+         * 문자열 결합 시 + 연산을 쓰면 매번 새로운 문자열 객체를 생성하므로 성능이 떨어질 수 있음
+         * 그래서 StringBuilder 사용: 문자열을 효율적으로 이어붙이기 위해
          */
+        StringBuilder sb = new StringBuilder();
+        
+        /*
+         * my_string을 m 글자씩 가로로 나열했을 때,
+         * c번째 열의 문자들을 세로로 읽는 문제.
+         * 
+         * 예시:
+         * my_string = "abcdefghijk"
+         * m = 3, c = 2
+         * 
+         * a b c
+         * d e f
+         * g h i
+         * j k
+         * 
+         * → c번째 열 = b, e, h, k
+         */
+        
+        // 인덱스는 0부터 시작하므로 c - 1이 첫 번째 열의 시작 위치
         for (int i = c - 1; i < my_string.length(); i += m) {
-            /*
-             * i번째 문자를 answer에 이어 붙임
-             * 문자열 + 연산은 매번 새로운 String 객체를 생성하므로 매우 긴 문자열의 경우 비효율적일 수 있음
-             */
-            answer += my_string.charAt(i);
+            // c번째 열의 문자를 StringBuilder에 추가
+            sb.append(my_string.charAt(i));
         }
         
-        // 완성된 문자열 반환
-        return answer;
+        // 최종적으로 완성된 문자열을 반환
+        return sb.toString();
     }
 }
