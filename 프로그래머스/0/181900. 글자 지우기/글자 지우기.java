11972710bd2b10ae1d8416915b20cf8268244ba8
mueiso <jcscene@gmail.com>
2025-08-16 22:04:33 +0900

    [level 0] Title: 글자 지우기, Time: 2.11 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,28 +1,17 @@
-import java.util.*;
-
 class Solution {
-    
+
     public String solution(String my_string, int[] indices) {
         
-        // StringBuilder는 문자열을 자주 수정(추가/삭제)할 때 성능이 좋음
-        StringBuilder sb = new StringBuilder();
-        
-        // 삭제할 인덱스를 빠르게 찾기 위해 boolean 배열 생성
-        boolean[] remove = new boolean[my_string.length()];
-        
-        // indices 배열에 있는 위치를 true로 표시
-        for (int idx : indices) {
-            remove[idx] = true;
+        String answer = "";
+        String[] tmp = my_string.split("");
+
+        for (int i = 0; i < indices.length; i++) {
+            tmp[indices[i]] = "";
         }
-        
-        // 문자열 순회하면서 삭제할 위치가 아니면 StringBuilder에 추가
-        for (int i = 0; i < my_string.length(); i++) {
-            if (!remove[i]) { // false라면 → 지우지 않는 문자
-                sb.append(my_string.charAt(i));
-            }
+
+        for (String x : tmp) {
+            answer += x;
         }
-        
-        // 최종 문자열 반환
-        return sb.toString();
+        return answer;
     }
 }
