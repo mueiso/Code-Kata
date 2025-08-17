cc96a150551f43a5c3c5b5f3d879e7a0e861ed45
mueiso <jcscene@gmail.com>
2025-08-18 02:01:27 +0900

    [level 0] Title: 리스트 자르기, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -1,32 +1,39 @@
-import java.util.*;
-
 class Solution {
     
     public int[] solution(int n, int[] slicer, int[] num_list) {
-        
-        int a = slicer[0];
-        int b = slicer[1];
-        int c = slicer[2];
-        
-        int[] answer = {};
-        
-        switch (n) {
-            case 1: // 0 ~ b
-                answer = Arrays.copyOfRange(num_list, 0, b + 1);
-                break;
-            case 2: // a ~ 끝까지
-                answer = Arrays.copyOfRange(num_list, a, num_list.length);
-                break;
-            case 3: // a ~ b
-                answer = Arrays.copyOfRange(num_list, a, b + 1);
-                break;
-            case 4: // a ~ b, step = c
-                List<Integer> list = new ArrayList<>();
-                for (int i = a; i <= b; i += c) {
-                    list.add(num_list[i]);
-                }
-                answer = list.stream().mapToInt(i -> i).toArray();
-                break;
+
+        /*
+         * n에 따라 슬라이싱 시작 인덱스를 결정
+         * n == 1 : num_list[0]부터 시작
+         * 나머지 : slicer[0]부터 시작
+         */
+        int start = n == 1 ? 0 : slicer[0];
+
+         /*
+         * n에 따라 슬라이싱 끝 인덱스를 결정
+         * n == 2 : num_list 마지막 인덱스까지
+         * 나머지 : slicer[1]까지
+         */
+        int end = n == 2 ? num_list.length - 1 : slicer[1];
+
+        /*
+         * n == 4일 때만 slicer[2] 간격(step) 사용
+         * 나머지는 step = 1
+         */
+        int step = n == 4 ? slicer[2] : 1;
+
+        /*
+         * 결과 배열 크기 계산
+         * (end - start + step) / step → 선택되는 원소 개수
+         */
+        int[] answer = new int[(end - start + step) / step];
+
+        /*
+         * 실제 슬라이싱 수행
+         * start부터 end까지 step 간격으로 num_list 요소를 가져와 answer에 저장
+         */
+        for (int i = start, j = 0; i <= end; i += step) {
+            answer[j++] = num_list[i];
         }
         
         return answer;
