24561dda6528592920cd3d92bbb03ca313ecfd1d
mueiso <jcscene@gmail.com>
2025-08-23 21:17:07 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.04 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -5,6 +5,8 @@ class Solution {
     public int[] solution(int[] num_list, int n) {
         
         // n번째 원소부터 끝까지 반환해야 하므로, 배열 인덱스 기준으로는 n-1부터 시작해야 함
-        return Arrays.copyOfRange(num_list, n - 1, num_list.length);
+        int[] a= Arrays.copyOfRange(num_list, n-1, num_list.length);
+        
+        return a;
     }
 }
