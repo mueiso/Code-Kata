d506923b0fecce59677860c9d30830ff666359da
mueiso <jcscene@gmail.com>
2025-08-26 00:32:33 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.02 ms, Memory: 94.3 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -1,18 +1,22 @@
 class Solution {
-    
     public int[] solution(int[] num_list, int n) {
-        
-        // 결과 배열의 크기 = num_list 길이를 n으로 나눈 값 (올림 처리)
-        int size = (num_list.length + n - 1) / n;
+        /*
+         * 전체 배열 길이에서 n 간격으로 뽑았을 때 몇 개의 원소가 나오는지 계산
+         * (ex: 길이가 10이고 n=3이면 → 10/3=3, 나머지가 있으므로 하나 더 → 4개)
+         */
+        int N = num_list.length % n == 0 ? num_list.length / n : num_list.length / n + 1;
+
+        // answer 배열에 원소를 담을 위치를 표시할 인덱스
+        int idx = 0;
 
         // 결과를 저장할 배열 선언
-        int[] answer = new int[size];
+        int[] answer = new int[N];
 
-        // n 간격으로 원소를 가져오기
-        for (int i = 0, j = 0; i < num_list.length; i += n, j++) {
-            answer[j] = num_list[i];  // i번째 원소를 answer에 순서대로 저장
+        // i를 n씩 증가시키면서 원소를 뽑아 answer에 저장
+        for (int i = 0; i < num_list.length; i += n) {
+            answer[idx++] = num_list[i];
         }
 
-        return answer;  // 결과 배열 반환
+        return answer;
     }
 }
