9773f41f4810611dd2a15d0ae13a1ad93c93f7dc
mueiso <jcscene@gmail.com>
2025-08-29 20:12:08 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 2.10 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -2,19 +2,25 @@ class Solution {
     
     public int[] solution(int[] arr, int[][] queries) {
         
-        // queries의 각 쿼리를 순서대로 처리
-        for (int[] query : queries) {
-            
-            int s = query[0]; // 시작 인덱스
-            int e = query[1]; // 끝 인덱스
-            
-            // s ≤ i ≤ e 범위에 대해 arr[i]에 1을 더함
-            for (int i = s; i <= e; i++) {
-                arr[i]++;
+        // 결과를 담을 배열(현재는 사용하지 않고 arr를 바로 반환)
+        int[] answer = {};
+
+        // queries 배열을 순회 (각 query = [s, e])
+        for(int i=0; i<queries.length; i++){
+
+            // query 범위: s부터 e까지 arr의 해당 구간 값을 +1
+            for(int j=queries[i][0]; j<=queries[i][1]; j++){
+                arr[j]++; // arr[j]의 값을 1 증가시킴
             }
         }
-        
-        // 모든 쿼리 처리가 끝난 후 arr를 반환
+
+        // 모든 query 처리가 끝난 후, 수정된 arr를 반환
         return arr;
     }
 }
+
+/*
+ 1. queries는 [s, e] 구간을 의미 → arr[s]부터 arr[e]까지의 원소를 +1 증가시킴
+ 2. 모든 쿼리를 차례대로 수행하면서 arr 배열을 직접 변경
+ 3. 최종적으로 arr를 반환
+ */
