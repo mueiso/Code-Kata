086fd914271d15310840600baf945442af6101fe
mueiso <jcscene@gmail.com>
2025-09-02 00:03:57 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.03 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,10 @@
+class Solution {
+    
+    public String solution(String myString) {
+        
+        // 문자열을 모두 대문자로 변환 (예: "hello" -> "HELLO")
+        String answer = myString.toUpperCase();
+        
+        return answer;
+    }
+}
