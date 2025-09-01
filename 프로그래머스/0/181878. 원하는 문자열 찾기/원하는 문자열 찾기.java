893e2602d25ebae50fc943700c22d31cbe55498f
mueiso <jcscene@gmail.com>
2025-09-01 15:52:05 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 11.29 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -1,16 +1,12 @@
 class Solution {
     
     public int solution(String myString, String pat) {
-        
-        // myString과 pat을 소문자로 변환 (대소문자 구분하지 않기 위해)
-        String lowerMyString = myString.toLowerCase();
-        String lowerPat = pat.toLowerCase();
-        
-        // 부분 문자열 존재 여부 확인
-        if (lowerMyString.contains(lowerPat)) {
-            return 1;
-        } else {
-            return 0;
-        }
+
+        /*
+         * myString과 pat을 소문자로 변환해서 포함 여부를 확인 -> 대소문자 구분 없이 비교 가능
+         * contains() 메서드는 문자열에 특정 부분 문자열이 존재하는지 확인 (있으면 true, 없으면 false)
+         * 삼항 연산자를 사용하여 true일 경우 1, false일 경우 0을 반환
+         */
+        return myString.toLowerCase().contains(pat.toLowerCase()) ? 1 : 0;
     }
 }
