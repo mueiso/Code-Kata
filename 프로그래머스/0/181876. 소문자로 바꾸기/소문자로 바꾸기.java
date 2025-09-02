03212f80af9cd75f47abdc93d56a0b8a826873ab
mueiso <jcscene@gmail.com>
2025-09-03 00:09:58 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.03 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,10 @@
+class Solution {
+    
+    public String solution(String myString) {
+        
+        // 문자열을 모두 소문자로 변환
+        String answer = myString.toLowerCase();
+        
+        return answer;
+    }
+}
