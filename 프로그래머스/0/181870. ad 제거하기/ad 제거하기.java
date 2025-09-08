c9b0c7e20061a87cdc0b61dc26823a64cc0a4f0f
mueiso <jcscene@gmail.com>
2025-09-09 01:29:23 +0900

    [level 0] Title: ad 제거하기, Time: 2.33 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -4,18 +4,8 @@ class Solution {
     
     public String[] solution(String[] strArr) {
         
-        // 결과를 담을 리스트
-        List<String> list = new ArrayList<>();
-        
-        // 주어진 문자열 배열 순회
-        for (String s : strArr) {
-            // "ad"가 포함되지 않은 문자열만 결과 리스트에 추가
-            if (!s.contains("ad")) {
-                list.add(s);
-            }
-        }
-        
-        // 리스트를 배열로 변환하여 반환
-        return list.toArray(new String[0]);
+        return Arrays.stream(strArr)         // 배열 → 스트림 변환
+                     .filter(s -> !s.contains("ad")) // "ad"가 포함되지 않은 문자열만 통과
+                     .toArray(String[]::new); // 최종 결과를 배열로 변환
     }
 }
