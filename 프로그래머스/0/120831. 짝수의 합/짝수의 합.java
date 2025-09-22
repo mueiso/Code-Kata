1d006f9805c1c2f78036590e6d75c7ba61eae915
mueiso <jcscene@gmail.com>
2025-09-22 11:15:09 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -2,14 +2,15 @@ public class Solution {
     
     public int solution(int n) {
         
-        int sum = 0;  // 합을 저장할 변수
+        // 합을 저장할 변수
+        int sum = 0;
         
         // 2부터 n까지 반복하면서 짝수만 더함
         for (int i = 2; i <= n; i += 2) {
             sum += i;
         }
         
-         // 최종 합 반환
+        // 최종 합 반환
         return sum;
     }
 }
