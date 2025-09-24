416cb94039a929248eb1d977cd11e47750dc07a8
mueiso <jcscene@gmail.com>
2025-09-24 10:58:18 +0900

    [level 1] Title: 평균 구하기, Time: 3.64 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,18 +1,10 @@
+import java.util.*;
+import java.lang.*;
+
 class Solution {
     
     public double solution(int[] arr) {
-        
-        // sum이라고 변수 선언하고 0으로 초기화
-        double sum = 0;
-        
-        // 배열의 모든 값 더하기
-        for (int num : arr) {
-            sum += num;
-        }
-        
-        // 배열의 평균값
-        double answer = sum / arr.length;
-        
-        return answer;
+
+        return Arrays.stream(arr).average().getAsDouble();
     }
 }
