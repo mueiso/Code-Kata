2d986955c4a5ebc1e7533b89b1c608d3767a7ad2
mueiso <jcscene@gmail.com>
2025-10-06 02:10:30 +0900

    [level 0] Title: 배열의 길이를 2의 거듭제곱으로 만들기, Time: 0.03 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.java
+++ 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.java
@@ -1,13 +1,26 @@
 /* [문제요약]
+
  * 정수 배열 arr이 주어짐
  * arr.length가 2의 거듭제곱(예: 1, 2, 4, 8, 16, …)이 되도록 뒤에 0을 최소 개수로 추가
  * 결과 배열을 반환
  */
 
 /* [문제풀이]
- * 현재 길이 len = arr.length를 구함
- * len보다 크거나 같으면서 가장 작은 2의 거듭제곱 값을 찾음 (예: nextPowerOfTwo)
- * nextPowerOfTwo - len만큼 0을 추가한 배열 생성
+
+ * 초기값
+   length = 1부터 시작합니다
+
+ * 2의 거듭제곱 계산
+   while (length < arr.length)
+
+   arr의 길이 이상이 될 때까지 length *= 2
+   결과적으로 arr.length보다 크거나 같은 최소의 2의 거듭제곱이 됩니다
+
+ * 배열 복사
+   Arrays.copyOf(arr, length)
+
+   arr의 원소를 그대로 복사하고, 남는 부분은 0으로 채움
+   별도의 반복문 없이 간단하게 처리 가능
  */
 
 import java.util.*;
@@ -16,24 +29,14 @@ class Solution {
     
     public int[] solution(int[] arr) {
         
-        int len = arr.length;
-        
-        // len보다 크거나 같은 가장 작은 2의 거듭제곱 찾기
-        int target = 1;
-        
-        while (target < len) {
-            target *= 2;
+        int length = 1;
+
+        // arr.length 이상이 되는 가장 작은 2의 거듭제곱 구하기
+        while (length < arr.length) {
+            length *= 2;
         }
 
-        // 결과 배열 생성
-        int[] answer = new int[target];
-        
-        // arr 복사
-        for (int i = 0; i < len; i++) {
-            answer[i] = arr[i];
-        }
-        
-        // 나머지 값은 기본적으로 0 (int 배열은 기본값이 0)
-        return answer;
+        // arr 복사 (남는 공간은 자동으로 0으로 채워짐)
+        return Arrays.copyOf(arr, length);
     }
 }
