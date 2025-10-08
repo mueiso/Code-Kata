617f2a8fa79ed139ceeadaffe39c7a4480e21a1d
mueiso <jcscene@gmail.com>
2025-10-09 02:45:58 +0900

    [level 0] Title: 배열의 길이에 따라 다른 연산하기, Time: 4.43 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.java
+++ 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.java
@@ -1,20 +1,22 @@
+import java.util.stream.IntStream;
+
 class Solution {
     
     public int[] solution(int[] arr, int n) {
         
         int len = arr.length;
-        
-        // arr 복사 (원본 배열 수정 방지)
-        int[] answer = arr.clone();
-        
-        // 홀수 길이 → 짝수 인덱스 수정
-        // 짝수 길이 → 홀수 인덱스 수정
-        int startIndex = (len % 2 == 1) ? 0 : 1;
 
-        for (int i = startIndex; i < len; i += 2) {
-            answer[i] += n;
-        }
-        
-        return answer;
+        return IntStream.range(0, len)
+                        // 인덱스 i에 대해 조건 적용
+                        .map(i -> {
+                            // 홀수 길이 배열이면 짝수 인덱스만 수정
+                            // 짝수 길이 배열이면 홀수 인덱스만 수정
+                            if ((len % 2 == 1 && i % 2 == 0) || (len % 2 == 0 && i % 2 == 1)) {
+                                return arr[i] + n;
+                            } else {
+                                return arr[i];
+                            }
+                        })
+                        .toArray();
     }
 }
