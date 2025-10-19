fc06e1dbffa5c3e75136655d278dace0e6595ff8
mueiso <jcscene@gmail.com>
2025-10-20 01:13:54 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 7.83 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,14 +1,15 @@
 class Solution {
+    
     public String solution(String[] seoul) {
         
-        // "Kim"의 위치 찾는 방법
-        int x = -1; // "Kim"의 위치 저장할 변수
+        // "Kim"의 위치 저장할 변수
+        int x = -1;
         
         // 배열 순회하면서 "Kim"의 인덱스 찾기
         for (int i = 0; i < seoul.length; i++) {
             if (seoul[i].equals("Kim")) {
                 x = i;
-                break; // "Kim" 찾으면 더 이상 반복할 필요 없다
+                break; // "Kim" 찾으면 break
             }
         }
 
