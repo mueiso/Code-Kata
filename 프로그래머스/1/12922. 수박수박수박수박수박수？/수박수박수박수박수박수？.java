73ec0449664dfd3d5be9c73dbc79c143ea75638e
mueiso <jcscene@gmail.com>
2025-10-29 11:17:38 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.15 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -2,17 +2,8 @@ public class Solution {
     
     public String solution(int n) {
         
-        // 문자열을 효율적으로 누적하기 위한 StringBuilder 사용
-        StringBuilder result = new StringBuilder();
-        // 반복 패턴 문자열 ("수박")
-        String pattern = "수박";
-
-        for (int i = 0; i < n; i++) {
-            // i % 2 : 짝수면 0, 홀수면 1
-            // pattern.charAt(0) → '수', pattern.charAt(1) → '박'
-            result.append(pattern.charAt(i % 2));   // "수박수박..." 순서대로 붙이기
-        }
-        
-        return result.toString();
+        // "수박"을 (n / 2 + 1)번 반복하여 충분히 긴 문자열을 만든 뒤
+        // substring(0, n)으로 원하는 길이만 잘라서 반환
+        return "수박".repeat((n / 2) + 1).substring(0, n);
     }
 }
