9d0467f39e92eb88f8842850aa3eb7fe1aaea6fe
mueiso <jcscene@gmail.com>
2025-10-30 00:10:28 +0900

    [level 1] Title: 내적, Time: 0.06 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -1,9 +1,15 @@
 class Solution {
+    
     public int solution(int[] a, int[] b) {
+        
+        // 내적 합계를 저장할 변수 초기화
         int answer = 0;
+        
+        // 두 배열의 각 인덱스를 순회하며 같은 위치의 원소끼리 곱해서 더함
         for (int i = 0; i < a.length; i++) {
-            answer += a[i] * b[i];  // 각 배열의 같은 인덱스를 곱한 값을 answer에 누적
+            answer += a[i] * b[i];  // a[i] * b[i]의 결과를 누적
         }
-        return answer;
+        
+        return answer; // 최종 내적 결과 반환
     }
 }
