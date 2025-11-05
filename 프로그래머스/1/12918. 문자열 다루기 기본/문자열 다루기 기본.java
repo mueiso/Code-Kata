7524f8e43d976c277320c3c62c396990e9a0872d
mueiso <jcscene@gmail.com>
2025-11-05 11:17:56 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.13 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,5 +1,7 @@
 class Solution {
+    
     public boolean solution(String s) {
+        
         // 문자열 길이가 4 또는 6인지 확인
         if (s.length() != 4 && s.length() != 6) {
             return false;
