9fc5a5f0cc0955a96e04627e402329d771fa6bba
mueiso <jcscene@gmail.com>
2025-11-06 11:15:42 +0900

    [level 1] Title: 행렬의 덧셈, Time: 6.22 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -2,17 +2,18 @@ class Solution {
     
     public int[][] solution(int[][] arr1, int[][] arr2) {
         
-        int rows = arr1.length;        // 행의 개수
-        int cols = arr1[0].length;     // 열의 개수
-        int[][] result = new int[rows][cols]; // 결과 행렬 생성
+        int rows = arr1.length;         // 행의 개수
+        int cols = arr1[0].length;      // 열의 개수
         
-        // 두 행렬의 같은 위치의 원소를 더하여 결과 행렬에 저장
+        int[][] answer = new int[rows][cols]; // 새로운 결과 배열 생성
+        
+        // 두 행렬의 같은 위치의 원소를 더하기
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                result[i][j] = arr1[i][j] + arr2[i][j];
+                answer[i][j] = arr1[i][j] + arr2[i][j];
             }
         }
         
-        return result; // 결과 반환
+        return answer;
     }
 }
