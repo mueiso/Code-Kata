bb53f8671243a4ca4ad9433b453f4122a4cd6c21
mueiso <jcscene@gmail.com>
2025-11-12 17:59:25 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.09 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,25 +1,23 @@
-public class Solution {
+class Solution {
     
-    public String solution(String s) {
-        
-        StringBuilder answer = new StringBuilder();
-        String[] words = s.split(" ", -1); // 공백 유지
-        
-        for (int i = 0; i < words.length; i++) {
-            String word = words[i];
-            for (int j = 0; j < word.length(); j++) {
-                char ch = word.charAt(j);
-                if (j % 2 == 0) {
-                    answer.append(Character.toUpperCase(ch));
-                } else {
-                    answer.append(Character.toLowerCase(ch));
-                }
-            }
-            if (i < words.length - 1) {
-                answer.append(" "); // 단어 사이의 공백 추가
-            }
+  public String solution(String s) {
+      
+        // 문자열을 문자 배열로 변환
+        char[] chars = s.toCharArray();
+        int idx = 0; // 각 단어 내에서의 문자 인덱스 (0부터 시작)
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == ' ')
+                // 공백을 만나면 새로운 단어 시작 → 인덱스 리셋
+                idx = 0;
+            else
+                // 단어 내에서의 짝수/홀수 인덱스에 따라 대문자/소문자 변환
+                chars[i] = (idx++ % 2 == 0)
+                           ? Character.toUpperCase(chars[i]) // 짝수번째 → 대문자
+                           : Character.toLowerCase(chars[i]); // 홀수번째 → 소문자
         }
-        
-        return answer.toString();
-    }
+
+        // 문자 배열을 다시 문자열로 변환하여 반환
+        return String.valueOf(chars);
+  }
 }
