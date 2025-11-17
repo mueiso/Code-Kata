0a41b0f36c38b25a1f88c1acecca5f0a9ed62b2a
mueiso <jcscene@gmail.com>
2025-11-17 17:59:55 +0900

    [level 1] Title: 최소직사각형, Time: 2.35 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,16 +1,21 @@
 class Solution {
+    
     public int solution(int[][] sizes) {
-        int maxWidth = 0;
-        int maxHeight = 0;
+        
+        int maxWidth = 0;   // 눕혔을 때의 '가로(짧은 변)' 중 최댓값
+        int maxHeight = 0;  // 눕혔을 때의 '세로(긴 변)' 중 최댓값
 
         for (int[] size : sizes) {
-            int width = Math.min(size[0], size[1]);
-            int height = Math.max(size[0], size[1]);
+            // 명함을 항상 짧은 변이 가로, 긴 변이 세로가 되도록 회전
+            int width  = Math.min(size[0], size[1]); // 가로(짧은 변)
+            int height = Math.max(size[0], size[1]); // 세로(긴 변)
 
-            maxWidth = Math.max(maxWidth, width);
+            // 각 축별 최댓값 갱신
+            maxWidth  = Math.max(maxWidth,  width);
             maxHeight = Math.max(maxHeight, height);
         }
 
+        // 가장 작은 지갑의 크기 = (가로 최댓값) × (세로 최댓값)
         return maxWidth * maxHeight;
     }
 }
