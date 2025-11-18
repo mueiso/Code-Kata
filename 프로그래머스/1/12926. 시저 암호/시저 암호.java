e3566d345b732b4fe4db702a2489f856fddaf3bd
mueiso <jcscene@gmail.com>
2025-11-18 12:30:21 +0900

    [level 1] Title: 시저 암호, Time: 26.70 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,20 +1,29 @@
 class Solution {
+    
     public String solution(String s, int n) {
-        String answer = "";
         
+        String answer = "";
+        // 문자열을 문자 배열로 변환
         char[] ch = s.toCharArray();
         
-        for(char c : ch) {
-            if(c == 32) answer += " ";
+        for (char c : ch) {
+            
+            // 공백(ASCII 32)은 그대로 유지
+            if (c == 32) {
+                answer += " ";
+            } 
             else {
-                if(c <= 90) {
+                // 대문자 범위 (A=65 ~ Z=90)
+                if (c <= 90) {
+                    c += n;              // n만큼 밀기
+                    if (c > 90) c -= 26; // 'Z'를 넘어가면 'A'로 순환
+                } 
+                // 소문자 범위 (a=97 ~ z=122)
+                else {
                     c += n;
-                    if(c > 90) c -= 26;
-                } else {
-                    c += n;
-                    if(c > 122) c -= 26;
+                    if (c > 122) c -= 26; // 'z'를 넘어가면 'a'로 순환
                 }
-                answer += c;
+                answer += c;              // 변환된 문자 추가
             }
         }
         
