db71fbbc687fd0c9301053a960cc8a610986de59
mueiso <jcscene@gmail.com>
2025-11-18 12:32:32 +0900

    [level 1] Title: 시저 암호, Time: 1.21 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,32 +1,39 @@
 class Solution {
     
     public String solution(String s, int n) {
+
+        // 문자열을 누적하기 위한 StringBuilder (+= 보다 성능 우수)
+        StringBuilder sb = new StringBuilder();
         
-        String answer = "";
-        // 문자열을 문자 배열로 변환
-        char[] ch = s.toCharArray();
-        
-        for (char c : ch) {
+        // 문자열을 문자 배열로 변환 후 하나씩 처리
+        for (char c : s.toCharArray()) {
             
-            // 공백(ASCII 32)은 그대로 유지
-            if (c == 32) {
-                answer += " ";
-            } 
-            else {
-                // 대문자 범위 (A=65 ~ Z=90)
-                if (c <= 90) {
-                    c += n;              // n만큼 밀기
-                    if (c > 90) c -= 26; // 'Z'를 넘어가면 'A'로 순환
+            // 공백은 암호화하지 않고 그대로 추가
+            if (c == ' ') {
+                sb.append(' ');
+            } else {
+
+                // 대문자인 경우 처리 ('A' ~ 'Z')
+                if (Character.isUpperCase(c)) {
+                    /*
+                     * 'A'를 기준으로 0~25 범위로 변환 후 n만큼 이동
+                     * 26으로 나눈 나머지를 구해 알파벳 순환 처리
+                     * 다시 'A'를 더해 문자로 복원
+                     */
+                    c = (char) ((c - 'A' + n) % 26 + 'A');
                 } 
-                // 소문자 범위 (a=97 ~ z=122)
+                // 소문자인 경우 처리 ('a' ~ 'z')
                 else {
-                    c += n;
-                    if (c > 122) c -= 26; // 'z'를 넘어가면 'a'로 순환
+                    // 'a' 기준으로 동일한 방식으로 순환 처리
+                    c = (char) ((c - 'a' + n) % 26 + 'a');
                 }
-                answer += c;              // 변환된 문자 추가
+
+                // 변환된 문자 누적
+                sb.append(c);
             }
         }
-        
-        return answer;
+
+        // 완성된 암호문 반환
+        return sb.toString();
     }
 }
