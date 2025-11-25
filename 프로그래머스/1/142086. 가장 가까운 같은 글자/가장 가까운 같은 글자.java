376214e31094a6c4e1eef023039d1fab42fd664d
mueiso <jcscene@gmail.com>
2025-11-25 10:56:28 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 47.17 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,32 +1,30 @@
-import java.util.*;
-
 class Solution {
     
-    public int[] solution(String s) {
-        
-        // 정답을 담을 배열 (각 인덱스별로 "가장 가까운 같은 문자까지의 거리" 저장)
-        int[] answer = new int[s.length()];
-        
-        // 각 문자별로 "마지막으로 등장한 인덱스"를 저장할 Map
-        Map<Character, Integer> lastSeen = new HashMap<>();
+    public int[] solution(String str) {
 
-        // 문자열을 왼쪽부터 오른쪽으로 순회
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i); // 현재 위치의 문자
-            
-            // 이전에 이 문자가 등장한 적이 있다면
-            if (lastSeen.containsKey(ch)) {
-                // (현재 인덱스 i) - (바로 앞에서 나왔던 같은 문자 인덱스)
-                answer[i] = i - lastSeen.get(ch);
-            } else {
-                // 처음 등장하는 문자라면 -1
-                answer[i] = -1;
+        // 정답을 저장할 배열 (각 인덱스별로 거리 저장)
+        int[] result = new int[str.length()];
+
+        // 문자열의 각 인덱스 i를 순회
+        for (int i = 0; i < str.length(); i++) {
+
+            // 0 ~ i-1 구간의 부분 문자열
+            String subStr = str.substring(0, i);
+
+            // 현재 문자
+            char current = str.charAt(i);
+
+            // 부분 문자열에서 현재 문자가 등장하지 않은 경우
+            if (subStr.indexOf(current) == -1) {
+                result[i] = -1;
+            } 
+            // 이전에 등장한 적이 있는 경우
+            else {
+                // 가장 최근에 등장한 위치(lastIndexOf)와의 거리 계산
+                result[i] = i - subStr.lastIndexOf(current);
             }
-            
-            // 이 문자의 "마지막 등장 위치"를 현재 인덱스로 갱신
-            lastSeen.put(ch, i);
         }
 
-        return answer;
+        return result;
     }
 }
