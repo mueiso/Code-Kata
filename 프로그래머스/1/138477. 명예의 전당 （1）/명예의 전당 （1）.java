4473c71f294eadc2aea5e450f796e3468727a3b2
mueiso <jcscene@gmail.com>
2025-11-28 11:18:18 +0900

    [level 1] Title: 명예의 전당 (1), Time: 3.26 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,20 +1,26 @@
 import java.util.*;
 
 class Solution {
+    
     public int[] solution(int k, int[] score) {
+
         int[] answer = new int[score.length];
-        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 최소 힙 (명예의 전당)
-        
+        PriorityQueue<Integer> hall = new PriorityQueue<>();  // 명예의 전당(최소 힙)
+
         for (int i = 0; i < score.length; i++) {
-            pq.offer(score[i]); // 점수 추가
-            
-            if (pq.size() > k) {
-                pq.poll(); // 명예의 전당에서 가장 낮은 점수 제거
+
+            // 오늘 점수 추가
+            hall.add(score[i]);
+
+            // 명예의 전당이 가득 차면, 가장 낮은 점수 제거
+            if (hall.size() > k) {
+                hall.poll();
             }
-            
-            answer[i] = pq.peek(); // 현재 명예의 전당 최하위 점수
+
+            // 현재 명예의 전당 중 가장 낮은 점수 기록
+            answer[i] = hall.peek();
         }
-        
+
         return answer;
     }
 }
