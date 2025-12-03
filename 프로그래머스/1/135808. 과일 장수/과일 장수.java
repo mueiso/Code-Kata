23209d5e44ca3b45e0ec6c9ce0f5151b1e252c13
mueiso <jcscene@gmail.com>
2025-12-03 12:47:57 +0900

    [level 1] Title: 과일 장수, Time: 66.70 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -4,15 +4,17 @@ class Solution {
     
     public int solution(int k, int m, int[] score) {
         
-        Arrays.sort(score); // 오름차순 정렬
-        int n = score.length;
-        int profit = 0;
+        int answer = 0;
 
-        // 뒤에서부터 m개씩 묶어서 최소 점수 * m만큼 수익을 계산
-        for (int i = n - m; i >= 0; i -= m) {
-            profit += score[i] * m;
+        Arrays.sort(score); // 오름차순 정렬
+
+        // i는 "상자에서 가장 낮은 점수의 인덱스"
+        // 뒤에서부터 m개씩 묶을 거니까 시작점은 length - m
+        for (int i = score.length - m; i >= 0; i -= m) {
+            // i가 현재 상자에서의 최소 점수 위치
+            answer += score[i] * m;
         }
 
-        return profit;
+        return answer;
     }
 }
