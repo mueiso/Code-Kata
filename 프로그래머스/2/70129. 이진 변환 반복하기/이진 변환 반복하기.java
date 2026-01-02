09f8984326f9c0276d36de07571895b03412532d
mueiso <jcscene@gmail.com>
2026-01-03 00:29:10 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 26.66 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -2,23 +2,31 @@ class Solution {
     
     public int[] solution(String s) {
         
-        int transformCount = 0; // 이진 변환 횟수
-        int removedZeroCount = 0; // 제거된 0의 총 개수
+        int[] answer = new int[2]; // [이진 변환 횟수, 제거된 0의 개수]
+        int temp;
 
         // s가 "1"이 될 때까지 반복
         while (!s.equals("1")) {
-            transformCount++;
 
-            int beforeLen = s.length();      // 변환 전 길이
-            s = s.replace("0", "");          // 0 제거
-            int afterLen = s.length();       // 0 제거 후 길이
+            // 현재 문자열 길이를 제거된 0 계산을 위해 더해둠
+            answer[1] += s.length();
 
-            removedZeroCount += (beforeLen - afterLen); // 이번에 제거된 0 누적
+            // 0 제거
+            s = s.replaceAll("0", "");
 
-            // 남은 문자열 길이를 2진수 문자열로 변환
-            s = Integer.toBinaryString(afterLen);
+            // 0 제거 후 길이
+            temp = s.length();
+
+            // 길이를 2진수 문자열로 변환
+            s = Integer.toBinaryString(temp);
+
+            // 변환 횟수 증가
+            answer[0]++;
+
+            // 제거되지 않은 1의 개수만큼 빼서 실제 제거된 0 개수 계산
+            answer[1] -= temp;
         }
 
-        return new int[]{transformCount, removedZeroCount};
+        return answer;
     }
 }
