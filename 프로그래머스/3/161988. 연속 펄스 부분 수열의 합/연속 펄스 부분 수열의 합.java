039853c02f234d8d9bcceedcf302eaa3b0d4e0d6
mueiso <jcscene@gmail.com>
2026-04-08 00:46:25 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 15.05 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -1,9 +1,3 @@
-/*
- * currentSum: 첫 번째 요소부터 현재 요소까지 [1, -1, 1, ...] 펄스를 곱하며 더한 누적 합이다.
- * maxPrefix & minPrefix: 누적 합의 그래프가 그려진다고 상상했을 때, 가장 높이 올라간 고점(maxPrefix)과 가장 깊이 내려간 저점(minPrefix)이다.
- * maxPrefix - minPrefix: 그래프의 가장 높은 곳과 낮은 곳의 낙차가 곧 우리가 얻을 수 있는 가장 큰 부분 수열의 합이 된다. 만약 음수 펄스가 정답이라면 그래프가 반대로 뒤집힐 뿐이므로 낙차는 동일하게 적용된다.
- */
-
 class Solution {
     
     public long solution(int[] sequence) {
