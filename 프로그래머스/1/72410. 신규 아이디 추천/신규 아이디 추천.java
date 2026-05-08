8b20c08396b560f84278289f2ece236927d20fa7
mueiso <jcscene@gmail.com>
2026-05-08 23:37:05 +0900

    [level 1] Title: 신규 아이디 추천, Time: 2.58 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -2,37 +2,99 @@ class Solution {
 
     public String solution(String new_id) {
 
-        // 1단계: 대문자 -> 소문자
-        String answer = new_id.toLowerCase();
+        // 7단계를 순차적으로 수행
+        return new KakaoId(new_id)
+                .toLowerCase()
+                .filter()
+                .compressDot()
+                .removeDot()
+                .fillEmpty()
+                .truncate()
+                .extend()
+                .get();
+    }
 
-        // 2단계: 허용 문자 제외 모두 제거
-        // 허용: 소문자, 숫자, -, _, .
-        answer = answer.replaceAll("[^a-z0-9-_.]", "");
+    private static class KakaoId {
+
+        private String value;
+
+        public KakaoId(String value) {
+            
+            this.value = value;
+        }
+
+        // 1단계: 대문자 -> 소문자
+        public KakaoId toLowerCase() {
+            
+            value = value.toLowerCase();
+            
+            return this;
+        }
+
+        // 2단계: 허용 문자 제외 제거
+        public KakaoId filter() {
+            
+            value = value.replaceAll("[^a-z0-9._-]", "");
+            
+            return this;
+        }
 
         // 3단계: 연속된 '.' -> '.'
-        answer = answer.replaceAll("[.]{2,}", ".");
+        public KakaoId compressDot() {
+            
+            value = value.replaceAll("\\.{2,}", ".");
+            
+            return this;
+        }
 
-        // 4단계: 처음이나 끝의 '.' 제거
-        answer = answer.replaceAll("^[.]|[.]$", "");
+        // 4단계: 처음과 끝의 '.' 제거
+        public KakaoId removeDot() {
+            
+            value = value.replaceAll("^\\.|\\.$", "");
+            
+            return this;
+        }
 
         // 5단계: 빈 문자열이면 "a"
-        if (answer.isEmpty()) {
-            answer = "a";
+        public KakaoId fillEmpty() {
+
+            if (value.isEmpty()) {
+                value = "a";
+            }
+
+            return this;
         }
 
-        // 6단계: 길이 15 초과면 자르기
-        if (answer.length() >= 16) {
-            answer = answer.substring(0, 15);
+        // 6단계: 길이 15 제한 + 끝 '.' 제거
+        public KakaoId truncate() {
+
+            if (value.length() >= 16) {
+                value = value.substring(0, 15);
+            }
+
+            value = value.replaceAll("\\.$", "");
+
+            return this;
         }
 
-        // 끝이 '.'이면 제거
-        answer = answer.replaceAll("[.]$", "");
+        // 7단계: 길이가 3이 될 때까지 마지막 문자 추가
+        public KakaoId extend() {
 
-        // 7단계: 길이가 2 이하이면 마지막 문자 반복
-        while (answer.length() < 3) {
-            answer += answer.charAt(answer.length() - 1);
+            StringBuilder sb = new StringBuilder(value);
+
+            while (sb.length() < 3) {
+                sb.append(sb.charAt(sb.length() - 1));
+            }
+
+            value = sb.toString();
+
+            return this;
         }
 
-        return answer;
+        // 최종 결과 반환
+        public String get() {
+            
+            return value;
+        }
     }
 }
